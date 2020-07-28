14cfcfef1f910565f7e2392684d15ec528d79a0f
ADABRAMS <67923657+ADABRAMS@users.noreply.github.com>
2020-07-28 12:31:09 -0400

    Update testYahtzee.py
    adding new code directly to github, as pycharm gives error "Remote: Permission to mstoth/Yahtzee.git denied to ADABRAMS. unable to access 'https://github.com/mstoth/Yahtzee/': The requested URL returned error: 403"

--- testYahtzee.py
+++ testYahtzee.py
@@ -1,69 +1,66 @@
-# importing from unittest and Die.  The unittest module has a class called TestCase.
-# There are lots of methods associated with that class that help to test your code.
-# Die.py has our Die class of course.
 import unittest
+
 from Die import Die
 
 
-# the beginning of our Yahtzee class.  This is our game.
-# right now there is the initialization routine __init__ which is called each time
-# you instantiate an instance of the class.
-# There is a difference in the class definition and the instantiation of it.
-# The class is like a template that you create an object from.
 class Yahtzee():
     def __init__(self):
-        self.chosen = ()  # This will keep the indices for the dice we don't want to roll again
-        self.d1 = Die(6)  # these are our 5 dice
+        self.chosen = ()
+        self.d1 = Die(6)
         self.d2 = Die(6)
         self.d3 = Die(6)
         self.d4 = Die(6)
         self.d5 = Die(6)
-        # we collect them in something called cup_of_dice
         self.cup_of_dice = [self.d1, self.d2, self.d3, self.d4, self.d5]
+        self.choices = {"Yahtzee": False}
 
-
-
+    def score(self, numbers):
+        a = numbers.count(numbers[0])
+        if a == 5:
+            return 50
+        else:
+            return numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]
         # figure out if numbers which is a list of 5 numbers between 1 and 6
         # if they are all the same and return 50 if that is the case
         # if not, return the sum of all the numbers
 
-    def roll(self):  # we call this to roll the dice
-        if len(self.chosen) == 0:  # If nothing is chosen to hold, roll them all
+    def roll(self):
+        if len(self.chosen) == 0:
             rolled_dice = [self.d1.roll(), self.d2.roll(), self.d3.roll(), self.d4.roll(), self.d5.roll()]
             return rolled_dice
-        else:  # disable the dice that we don't want to roll
-            for v in self.chosen:  # v will be an index into cup_of_dice
-                self.cup_of_dice[v].active = False  # when a die is not active, it just keeps its value
+        else:
+            for v in self.chosen:
+                self.cup_of_dice[v].active = False
             rolled_dice = [self.d1.roll(), self.d2.roll(), self.d3.roll(), self.d4.roll(), self.d5.roll()]
             return rolled_dice
 
-    def choose(self, choice):  # choice will be a tuple
-        self.chosen = choice  # save it into the chosen variable.
+    def choose(self, choice):
+        self.chosen = choice
 
-    def score(self, values):
-        pass
-
-
-# this is our test case
-# it inherits the methods from unittest.TestCase
-# methods like assertTrue (you need self.assertTrue since it's part of the unittest.TestCase class)
-def self(args):
-    pass
+    def newchoose(self, key, myroll):
+        if myroll.count(myroll[0]) == 5:
+            if self.choices[key] == True:
+                return False
+            else:
+                self.choices[key] = True
+                return True
+        else:
+            return False
 
 
 class MyTestCase(unittest.TestCase):
     def test_yahtzee(self):
-        self.game = Yahtzee()  # just make sure we can create the game
+        self.game = Yahtzee()
 
     def test_roll(self):
         self.game = Yahtzee()
         values = self.game.roll()
-        self.assertEqual(5, len(values))  # make sure we get 5 values from the roll
+        self.assertEqual(5, len(values))
 
     def test_die(self):
         d = Die(6)
         v = d.roll()
-        self.assertGreater(v, 0)  # make sure the values are between 0 and 7
+        self.assertGreater(v, 0)
         self.assertLess(v, 7)
 
     def test_choose(self):
@@ -73,25 +70,24 @@ class MyTestCase(unittest.TestCase):
         self.game.choose((0, 1))
         new_values = self.game.roll()
         print(new_values)
-        self.assertEqual(values[0], new_values[0])  # make sure the first two values didn't change
-        self.assertEqual(values[1], new_values[1])
+        self.assertEquals(values[0], new_values[0])
+        self.assertEquals(values[1], new_values[1])
 
-    def score(self):
+    def test_newchoose(self):
+        self.game = Yahtzee()
+        values = [1, 1, 1, 1, 1]
+        self.assertFalse(self.game.newchoose("Yahtzee", [1, 2, 3, 4, 5]))
+        self.assertTrue(self.game.newchoose("Yahtzee", values))
+        self.assertFalse(self.game.newchoose("Yahtzee", values))
+        self.assertTrue(self.game.newchoose("Chance", [1, 2, 3, 4, 5]))
+        self.assertFalse(self.game.newchoose("Chance", [1, 2, 3, 4, 5]))
+
+    def test_score(self):
         self.game = Yahtzee()
         values = [1, 1, 1, 1, 1]
         self.assertTrue(self.game.score(values) == 50)
         values = [1, 2, 3, 4, 5]
-        self.assertTrue(self.game.score(values) != sum)
-
-    def sum(Numbers) -> object:
-        total = 0
-        for Die in Numbers:
-            total += Die
-        return total
-
-    def checkEqual(iterator):
-        return len(set(iterator)) <= 1
-
+        self.assertTrue(self.game.score(values) == 15)
 
 
 if __name__ == '__main__':
